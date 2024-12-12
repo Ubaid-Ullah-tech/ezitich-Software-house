68217c3a0f2b6acae402d86b87f5752b6969bec1
ubaid ullah <99728959+Ubaid-Ullah-tech@users.noreply.github.com>
2024-12-12 22:22:21 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = withMT({
+import withMT from "@material-tailwind/react/utils/withMT";
+export default withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -15,31 +15,3 @@ module.exports = withMT({
   },
   plugins: [],
 });
-
-
-
-
-
-
-
-
-
-
-
-
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         inter: ["Inter", "sans-serif"],
-//       },
-//     },
-//     screens: {
-//       // => @media (max-width: 1279px) { ... }
-//       lg: { min: "925px" },
-//     },
-//   },
-//   plugins: [],
-// };
